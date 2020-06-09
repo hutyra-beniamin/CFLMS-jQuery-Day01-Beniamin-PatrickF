d2d01000108be66337cc2d78ac63026148419f85
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-09 11:44:15 +0200

    finishes intermediate exercise

--- Intermediate/manipulating_html_page_exercise_files/myscript.js
+++ Intermediate/manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,22 @@
 $("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
-	
+
+});
+
+$(document).ready(function() {
+
+	//TASK 3
+	$("input").val("Search for...")
+	//TASK 2
+	for (i = 0; i < 5; i++) {
+		$("article").append("<div></div>")
+	}
+	//TASK 1
+	$("form").remove()
+	//TASK 4
+	$("article div").addClass("box")
+	//TASK 5
+	$("a").attr("href", "www.codefactory.wien")
 
 });
